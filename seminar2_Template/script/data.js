cba61c4418e753c3f4b99f1a6b056f930622232f
Denis Fedorov <ssboshen@yandex.ru>
2023-06-14 13:07:44 +0300

    Template HW based on 6 cards

---
+++ seminar2_Template/script/data.js
@@ -0,0 +1,38 @@
+const dataList = `[
+  {
+    "url": "img/products/men_with_bear_half.png",
+    "title": "ELLERY X M'O CAPSULE",
+    "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "52.00"
+  },
+  {
+    "url": "img/products/woman_in_black_full.png",
+    "title": "ELLERY X M'O CAPSULE",
+    "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "52.00"
+  },
+  {
+    "url": "img/products/young_men_in_hat.png",
+    "title": "ELLERY X M'O CAPSULE",
+    "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "52.00"
+  },
+  {
+    "url": "img/products/men_with_bear_full.png",
+    "title": "ELLERY X M'O CAPSULE",
+    "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "52.00"
+  },
+  {
+    "url": "img/products/woman_in_blue_half.png",
+    "title": "ELLERY X M'O CAPSULE",
+    "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "52.00"
+  },
+  {
+    "url": "img/products/dark_woman_half.png",
+    "title": "ELLERY X M'O CAPSULE",
+    "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "52.00"
+  }
+]`
